137ef311cc4f984aa8bba0d6799f168e3afc09ff
Evan Burton <butonevan1@gmaail.com>
2021-12-01 13:21:05 -0700

    readding index code not showing in vscode when pulled

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,7 +3,7 @@
   <nav class="bg-black p-5 flex justify-evenly">
       
   
-<ul class="text-3xl text-white flex space-x-8 truncate align-baseline">
+<ul class="text-3xl text-white flex flex-wrap space-x-8 text-center align-baseline">
       <li><NuxtLink class="hover:text-pink-600" to="/">Home</NuxtLink></li>
       <li><NuxtLink class="hover:text-pink-600" to="/blog">Content</NuxtLink></li>
       <li><NuxtLink class="hover:text-pink-600" to="/projects">Showcase</NuxtLink></li>
