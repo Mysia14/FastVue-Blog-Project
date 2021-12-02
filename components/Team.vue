9571468885fa87173da002994e22ee8be1199288
Jean-Marc <jmarcder@gmail.com>
2021-12-02 03:52:25 -0700

    Update Team.vue

--- components/Team.vue
+++ components/Team.vue
@@ -6,7 +6,7 @@
     <figure>
       <h3 class="text-5xl p-2">{{ memberInfo.name }}</h3>
       <a :href="memberInfo.link" target="_blank">
-        <nuxt-img class="rounded-full border-solid border-4 border-black" :src="memberInfo.image" sizes="sm:300px md:300px lg:300px" />
+        <nuxt-img class="rounded-full border-solid border-4 border-black" :src="memberInfo.image" sizes="sm:200px md:200px lg:200px" />
       </a>
       <figcaption class="text-3xl" v-text="memberInfo.role" />
     </figure>
