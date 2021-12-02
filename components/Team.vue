3fb61727234b330422f9939bbcb82a0fac1431bc
Evan Burton <butonevan1@gmaail.com>
2021-12-02 11:36:10 -0700

    fixed jean-marcs photo on sever, undid changes to css

--- components/Team.vue
+++ components/Team.vue
@@ -3,10 +3,10 @@
 
 
 
-    <figure>
+    <figure class="">
       <h3 class="text-5xl p-2">{{ memberInfo.name }}</h3>
       <a :href="memberInfo.link" target="_blank">
-        <nuxt-img class="rounded-full border-solid border-4 border-black" :src="memberInfo.image" sizes="sm:200px md:200px lg:200px" />
+        <nuxt-img class="rounded-full border-solid border-4 border-black" :src="memberInfo.image" sizes="sm:300px md:300px lg:300px" />
       </a>
       <figcaption class="text-3xl" v-text="memberInfo.role" />
     </figure>
