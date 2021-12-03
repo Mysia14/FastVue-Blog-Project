85276dd6f1d3bdfa611ffb821b36b429c511332a
FesalBadday <fesal.n04@hotmail.com>
2021-12-03 12:58:38 -0700

    added hamburger nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,9 +1,6 @@
 <template>
-    
   <nav class="bg-black p-5 flex justify-evenly">
-      
-  
-<ul class="text-3xl text-white flex flex-wrap space-x-8 text-center align-baseline">
+    <ul class="text-3xl text-white flex flex-wrap space-x-8 text-center align-baseline">
       <li><NuxtLink class="hover:text-pink-600" to="/">Home</NuxtLink></li>
       <li><NuxtLink class="hover:text-pink-600" to="/blog">Content</NuxtLink></li>
       <li><NuxtLink class="hover:text-pink-600" to="/projects">Showcase</NuxtLink></li>
@@ -12,3 +9,17 @@
     </ul>
   </nav>
 </template>
+
+<script>
+new Vue({
+	el: '#app',
+  data: {
+  	open: false,
+  },
+  methods: {
+  	toggle() {
+    	this.open = !this.open
+    }
+  }
+})
+</script>
